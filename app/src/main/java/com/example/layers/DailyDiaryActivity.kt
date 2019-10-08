5bce02d6ed1c41bb0bb23384ebe5528c42c3b650
Codeguru65 <winston.marava@gmail.com>
2019-10-08 13:09:45 +0100

    initial commit

---
+++ app/src/main/java/com/example/layers/DailyDiaryActivity.kt
@@ -0,0 +1,23 @@
+package com.example.layers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_daily_diary.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class DailyDiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_diary)
+
+
+
+    }
+}
